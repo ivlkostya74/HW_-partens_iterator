841fea8328f0b2854312735569ba206d2a2731a5
ivlkostya74 <ivlkostya@yandex.ru>
2022-07-08 09:29:16 +1000

    first commit

---
+++ src/Homwork/Randoms.java
@@ -0,0 +1,34 @@
+package Homwork;
+
+
+import java.util.Iterator;
+import java.util.PrimitiveIterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected PrimitiveIterator.OfInt rndIterator;
+    protected int min, max;
+
+    public Randoms(int min, int max) {
+        rndIterator = new Random().ints(min, (max + 1)).iterator();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return rndIterator.nextInt();
+            }
+
+    };
+    }
+
+}
